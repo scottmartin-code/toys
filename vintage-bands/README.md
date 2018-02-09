cad12f3b6be65db08fe598ba9da94b47ab0b3832
Scott Martin <scott@scottmartin.systems>
2018-02-09 10:42:02 +0000

    Initial commit

---
+++ vintage-bands/README.md
@@ -0,0 +1,3 @@
+# Vintage Band Name Generator
+
+Generate the kinds of names you see on 78RPM records.
---
+++ vintage-bands/vintage-bands.js
@@ -0,0 +1,180 @@
+makeName();
+
+document.getElementById('generate-button').addEventListener('click', () => {
+	makeName();
+});
+
+function makeName () {
+	document.getElementById('result').innerHTML = groupName();
+}
+
+// To do: take chance as parameter and merge in itemChance
+function randomItem(items) {
+	let r = Math.floor(Math.random() * items.length);
+	return items[r];
+}
+
+function itemChance (item, chance) {
+	let items = [];
+	items.push(item);
+	return [items, ...emptyItems(chance)];
+}
+
+function emptyItems (number) {
+	let items = [];
+	for (let i = 1; i < number; i++) {
+		items.push('');
+	}
+	return items;
+}
+
+function groupName () {
+	return leaderOrLeadersPossessive() + groupLabel();
+}
+
+function leaderOrLeadersPossessive () {
+	let outputs = [
+		() => { return leaderAndHis() },
+		() => { return leadersAndTheir() }
+	];
+
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r]();
+}
+
+function leaderAndHis () {
+	return leaderName() + randomItem([' and his ', '\'s ']);
+}
+
+function leadersAndTheir() {
+	return leaders() + ' and their ';
+}
+
+function leaders () {
+	let outputs = [
+		() => { return leaderName() + ' and ' + leaderName() },
+		() => { return "The King's Jesters" }
+	];
+
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r]();
+}
+
+function leaderName () {
+	let names = [
+		() => { return firstName() + ' ' + lastName() },
+		() => { return firstName() + ' ' + middleInitialOrNickname()
+			+ ' ' + lastName() }
+	];
+
+	let r = Math.floor(Math.random() * names.length);
+
+	// To do: don't return both adjective and middle initial
+	return adjective() + names[r]() + honorific();
+}
+
+function adjective () {
+	let outputs = itemChance("Smilin' ", 20);
+	// add: General
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r];
+}
+
+function firstName () {
+	return randomItem([
+		'Albert', 'Bill', 'Buddy', 'Clyde', 'Donnie', 'Edward', 'Frank',
+		'Gene', 'George', 'Glen', 'Hector', 'Johnnie', 'Karl', 'Louis', 'Ray',
+		'Sammy', 'Scotty', 'Sonny', 'Thomas', 'Vaughn', 'Wilbert', 'Wilson'
+	]);
+}
+
+function middleInitialOrNickname () {
+	return randomItem([
+		'A.', 'B.', 'C.', 'D.', 'E.', 'F.', 'G.', 'H.', 'I.', 'J.',
+		'K.', 'L.', 'M.', 'N.', 'O.', 'P.', 'Q.', 'R.', 'S.', 'T.',
+		'U.', 'V.',	'W.', 'X.', 'Y.', 'Z.', '"Red"', ...emptyItems(27)
+	]);
+}
+
+function lastName () {
+	return randomItem([
+		'Adams', 'Balmer', 'Baranco', 'Baur', 'Bostic', 'Carlson', 'Fox',
+		'Jenkins', 'Johnson', 'Kreisler', 'Kryger', 'Lewis', 'McCoy',
+		'McDonald',	'McPartland', 'Miller', 'Mooney', 'Short', 'Williams'
+	]);
+}
+
+function honorific () {
+	return randomItem(itemChance(' "King of the Polkas"', 20));
+}
+
+function groupLabel () {
+	let outputs = [
+		() => { return memberNouns() },
+		() => { return memberNounPrefix() + ' ' + memberNouns() },
+		() => { return orchestra() },
+		() => { return randomItem([
+			'Band', 'Ensemble', 'Trio', 'Quartet', 'Quintet', 'Sextet'
+			]) },
+	];
+
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r]();
+}
+
+function memberNounPrefix () {
+	return randomItem([
+		'Blue Grass', 'Buster Brown', 'Cactus', 'City', 'Famous', 'Farm',
+		'Fox', 'Jazz', 'Marimba', 'Motor City', 'Rambling', 'Red River',
+		'Rhythm', 'Royal', 'Statler', 'Tivoli', 'Valley'
+	]);
+}
+
+function memberNouns () {
+	return randomItem([
+		'Band', 'Bombadiers', 'Boys', 'Canadians', 'Chicagoans', 'Cowboys',
+		'Five', 'Gang', 'Hunters', 'Islanders', 'Pennsylvanians', 'Slickers',
+		'Syncopaters', 'Troopers', 'Twisters', 'Vagabonds', 'Yanks'
+	]);
+}
+
+function orchestra () {
+	return orchestraPlace() + orchestraSpecialty()
+		+ ' Orchestra' + orchestraSuffix();
+}
+
+function orchestraArticle () {
+	return randomItem(itemChance('The ', 2));
+}
+
+function orchestraPlace () {
+	return randomItem(itemChance('Casa Loma ', 10));
+}
+
+function orchestraSpecialty () {
+	return orchestraSpecialtyPrefix() + orchestraSpecialtyMain();
+}
+
+function orchestraSpecialtyPrefix () {
+	let outputs = [
+		() => { return randomItem(itemChance('International ', 10)) },
+		() => { return randomItem(itemChance('Specialty ', 10)) }
+	];
+
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r]();
+}
+
+function orchestraSpecialtyMain () {
+	let outputs = [
+		() => { return randomItem(itemChance('Dance ', 10)) },
+		() => { return randomItem(itemChance('Concert ', 10)) }
+	];
+
+	let r = Math.floor(Math.random() * outputs.length);
+	return outputs[r]();
+}
+
+function orchestraSuffix () {
+	return randomItem(itemChance(' De Luxe', 10));
+}
