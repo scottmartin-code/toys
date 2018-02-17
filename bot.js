ab28ce3adc33f69e5dd22332ab2ab74890487c0c
Scott Martin <scott@scottmartin.systems>
2018-02-17 16:43:00 +0000

    Use environment variables for authentication if available

--- bot.js
+++ bot.js
@@ -2,17 +2,30 @@
 
 // Based on Daniel Shiffman's tutorial: http://shiffman.net/a2z/twitter-bots/
 let Twit = require('twit');
-const config = require('./config.js');
-let T = new Twit(config);
+
+let twitter;
+
+if (process.env.consumer_key) {
+	twitter = new Twit({
+		consumer_key:        process.env.consumer_key,
+		consumer_secret:     process.env.consumer_secret,
+		access_token:        process.env.access_token,
+		access_token_secret: process.env.access_token_secret
+	});
+} else {
+	const config = require('./config.js');
+	twitter = new Twit(config);
+}
 
 let band = require('./vintage-bands');
 
+// Post on startup
 tweet();
 
 setInterval(tweet, 60*60*1000);
 
 function tweet () {
-	T.post('statuses/update', { status: band() }, tweeted);
+	twitter.post('statuses/update', { status: band() }, tweeted);
 
 	function tweeted(err, data, response) {
 		if (err) {
@@ -20,5 +33,5 @@ function tweet () {
 		} else {
 			console.log('Tweeted: ' + data.text);
 		}
-	};
+	}
 }
