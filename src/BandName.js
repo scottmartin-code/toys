414efc12022c56b30546228ba3ec65f4961aa3f9
Scott Martin <scott@scottmartin.systems>
2018-03-19 18:35:27 +0000

    Merge random into makeRecipe! Pow, completely refactored

--- src/BandName.js
+++ src/BandName.js
@@ -2,26 +2,10 @@ const recipes = require('./RecipeData.js');
 
 module.exports = () => { return makeRecipe('bandName') }
 
-function random (items, chance, space) {
-	let outputs;
-
-	if (chance) {
-		let spaced = items.map(item => {
-			return space == 'before' ? ` ${item}` : `${item} `;
-		});
-
-		outputs = [...spaced, ...Array.from({length: chance}, (v, i) => '')];
-	} else {
-		outputs = [...items];
-	}
-
-	return outputs[Math.floor(Math.random() * outputs.length)];
-}
-
 function makeRecipe (name) {
 	const recipe = recipes[name];
 
-	let possibleOutputs = recipe.items.map( item => {
+	let recipeItems = recipe.items.map( item => {
 		if (typeof item == 'object') {
 			return item.reduce( (acc, r) => {
 				return recipes[r] ? acc + makeRecipe(r) : acc + r;
@@ -31,5 +15,17 @@ function makeRecipe (name) {
 		}
 	});
 
-	return random(possibleOutputs, recipe.chance, recipe.space);
+	let out;
+
+	if (recipe.chance) {
+		let spaced = recipeItems.map(item => {
+			return recipe.space == 'before' ? ` ${item}` : `${item} `;
+		});
+
+		out = [...spaced, ...Array.from({length: recipe.chance}, (v, i) => '')];
+	} else {
+		out = [...recipeItems];
+	}
+
+	return out[Math.floor(Math.random() * out.length)];
 }
