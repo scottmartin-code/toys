de8ae1f72f350a0c7a86dc6b00aa0852bf64a9a9
Scott Martin <scott@scottmartin.systems>
2018-04-08 23:52:26 +0100

    Note plan to have random record label images in web version

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,8 @@ export const makeBandName = () => {
 		return dispatch(receiveBand(bandName()));
 	};
 };
+
+// To do: random label image and associated text styling. The Staunton
+// collection has hundreds, of which many are now public domain
+// http://www.tedstaunton.com/site_map/site_map.html
+// https://en.wikipedia.org/wiki/List_of_countries%27_copyright_lengths
