26a2c173c3a866012c1a0e5f381037e6b36f6ffa
Scott Martin <scott@scottmartin.systems>
2018-04-08 23:32:00 +0100

    Only tweet in production; upper case env vars

--- bot/bot.js
+++ bot/bot.js
@@ -1,16 +1,21 @@
 'use strict';
 
-const Twit     = require('twit');
-const bandName = require('../src/BandName.js');
+const Twit = require('twit');
+const { bandName } = require('../src/BandName.js');
+
+if (!process.env.PRODUCTION) {
+	console.log(bandName());
+	process.exit(0);
+}
 
 let twitter;
 
 try {
 	twitter = new Twit({
-		consumer_key:        process.env.consumer_key,
-		consumer_secret:     process.env.consumer_secret,
-		access_token:        process.env.access_token,
-		access_token_secret: process.env.access_token_secret,
+		consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+		access_token:        process.env.TWITTER_ACCESS_TOKEN,
+		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 	});
 } catch (err) {
 	throw new Error('Problem initializing Twit:', error.message);
